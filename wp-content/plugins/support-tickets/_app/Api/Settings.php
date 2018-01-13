2090910be1464c6b6b031fdaecd45365fa938b06
Connor Miotk <support@miotk.io>
2018-01-13 12:45:19 +0000

    (feature) be able ot do with sub pages

--- wp-content/plugins/support-tickets/_app/Api/Settings.php
+++ wp-content/plugins/support-tickets/_app/Api/Settings.php
@@ -8,6 +8,7 @@ namespace App\Api;
 class Settings
 {
     private $admin_pages = array();
+    private $admin_subpages = array();
 
     /**
      * Bootstrap the App\Api\Settings class.
@@ -32,6 +33,35 @@ class Settings
     }
 
     /**
+     * Generate a subpage along with an admin page.
+     * @param string|null $title
+     * @return $this
+     */
+    public function withSubPage(string $title = null)
+    {
+        if (empty($this->admin_pages)) {
+            return $this;
+        }
+
+        $admin_page = $this->admin_pages[0];
+
+        $subpage = [
+            [
+                'parent_slug' => $admin_page['menu_slug'],
+                'page_title' => $admin_page['page_title'],
+                'menu_title' => $admin_page['menu_title'],
+                'capability' => $admin_page['capability'],
+                'menu_slug' => $admin_page['menu_slug'],
+                'callback' => function() { echo 'subpage'; }
+            ]
+        ];
+
+        $this->admin_subpages = $subpage;
+
+        return $this;
+    }
+
+    /**
      * Loop through all of the pages and generate the menu page using the add_menu_page function.
      * @see add_menu_page()
      */
@@ -40,6 +70,11 @@ class Settings
         foreach($this->admin_pages as $page) {
             add_menu_page($page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'],
                           $page['callback'], $page['icon_url'], $page['position']);
+        }
+
+        foreach($this->admin_subpages as $page) {
+            add_submenu_page($page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'],
+                $page['menu_slug'], $page['callback']);
         }
     }
 }
