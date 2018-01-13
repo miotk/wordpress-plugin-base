92499e07405e7227ccd0c4568cf08c6fcc3c9db0
Connor Miotk <support@miotk.io>
2018-01-13 12:26:50 +0000

    (chore) tidying up admin.php file and making it nicer.

--- wp-content/plugins/support-tickets/_app/Pages/Admin.php
+++ wp-content/plugins/support-tickets/_app/Pages/Admin.php
@@ -11,8 +11,11 @@ use App\Bootstrap\Base;
 class Admin extends Base
 {
     public $settings;
-    private $pages;
+    private $pages = array();
 
+    /**
+     * Admin constructor.
+     */
     public function __construct()
     {
         parent::__construct();
@@ -24,27 +27,51 @@ class Admin extends Base
      */
     public function bootstrap()
     {
-        $pages = [
-            [
-                'page_title' => 'Support Tickets Settings',
-                'menu_title' => 'ST Settings',
-                'capability' =>  'manage_options',
-                'menu_slug' => 'support_tickets_settings',
-                'callback' => function() { echo 'test'; },
-                'icon_url' => 'dashicons-admin-tools',
-                'position' => 110
-            ],
-            [
-                'page_title' => 'Support Tickets',
-                'menu_title' => 'Support Tickets',
-                'capability' => 'manage_options',
-                'menu_slug' => 'support_tickets',
-                'callback' => function() { echo 'Support Tickets Page'; },
-                'icon_url' => 'dashicons-tickets-alt',
-                'position' => 6
-            ]
-        ];
+        $this->settingsPage();
+        $this->ticketsPage();
 
-        $this->settings->generatePages($pages)->bootstrap();
+        $this->settings->generatePages([$this->pages])->bootstrap();
+    }
+
+    /**
+     * Merge the new array with the existing pages array.
+     * @param $page
+     * @return array
+     */
+    private function addPage($page)
+    {
+        return $this->pages = array_merge($this->pages, $page);
+    }
+
+    /**
+     * Generate the settings page.
+     */
+    private function settingsPage()
+    {
+        $this->addPage([
+            'page_title' => 'Support Tickets Settings',
+            'menu_title' => 'ST Settings',
+            'capability' =>  'manage_options',
+            'menu_slug' => 'support_tickets_settings',
+            'callback' => function() { echo 'test'; },
+            'icon_url' => 'dashicons-admin-tools',
+            'position' => 110
+        ]);
+    }
+
+    /**
+     * Generate the tickets page.
+     */
+    private function ticketsPage()
+    {
+        $this->addPage([
+            'page_title' => 'Support Tickets',
+            'menu_title' => 'Support Tickets',
+            'capability' => 'manage_options',
+            'menu_slug' => 'support_tickets',
+            'callback' => function() { echo 'Support Tickets Page'; },
+            'icon_url' => 'dashicons-tickets-alt',
+            'position' => 6
+        ]);
     }
 }
